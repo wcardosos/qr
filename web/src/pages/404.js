afc392f1740d9b3dc70435492a9f8320678489dd
Wagner Cardoso <wagnerdev01@gmail.com>
2024-07-13 20:35:25 -0300

    chore(web): initial website project

---
+++ web/src/pages/404.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const NotFoundPage = () => {
+  return (
+    <main>
+      <p>The page that you try to access does not exists</p>
+    </main>
+  )
+}
+
+export default NotFoundPage
+
+export const Head = () => <title>Not found</title>
