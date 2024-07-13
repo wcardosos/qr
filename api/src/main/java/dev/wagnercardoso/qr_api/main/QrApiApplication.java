1aa370b15f4460756cb734e5510f003c723260b8
Wagner Cardoso <wagnerdev01@gmail.com>
2024-07-12 22:36:20 -0300

    chore(api): initial API project

---
+++ api/src/main/java/dev/wagnercardoso/qr_api/main/QrApiApplication.java
@@ -0,0 +1,13 @@
+package dev.wagnercardoso.qr_api.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/wagnercardoso/qr_api/main/QrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wagnercardoso.qr_api.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
